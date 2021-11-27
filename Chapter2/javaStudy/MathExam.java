d63f203b21ab9f5fefb329b0432513bc148d2685
Minho Ji <59356172+minho4418@users.noreply.github.com>
2021-11-28 00:28:46 +0900

    2. Math

---
+++ Chapter2/javaStudy/MathExam.java
@@ -0,0 +1,13 @@
+package javaStudy;
+
+public class MathExam {
+    public static void main(String[] args){
+        int value1 = Math.max(5,30);
+        System.out.println(value1);
+        int value2 = Math.min(5,30);
+        System.out.println(value2);
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.random());
+        System.out.println(Math.sqrt(25));
+    }
+}
