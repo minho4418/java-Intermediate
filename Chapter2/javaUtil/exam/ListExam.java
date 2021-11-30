b4d472565f9b6b509f28d734510477836c6d48bc
Minho Ji <59356172+minho4418@users.noreply.github.com>
2021-11-30 23:21:28 +0900

    2. Set, List

---
+++ Chapter2/javaUtil/exam/ListExam.java
@@ -0,0 +1,20 @@
+package javaUtil.exam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExam { // 리스트 : 저장공간이 필요에 따라 자유
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        list.add("kim");
+        list.add("kang");
+        list.add("kim");
+
+        System.out.println(list.size());
+
+        for(int i = 0; i < list.size(); i++){
+            String str = list.get(i);
+            System.out.println(str);
+        }
+    }
+}
