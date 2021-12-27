b310de721944efa661ae2c9c963b761c293464fc
Minho Ji <59356172+minho4418@users.noreply.github.com>
2021-12-27 23:30:11 +0900

    5. 쓰레드 만들기2

---
+++ Chapter5/javaUtil/exam/MyThread2.java
@@ -0,0 +1,19 @@
+package javaUtil.exam;
+
+public class MyThread2 implements Runnable {
+    String str;
+    public MyThread2(String str){
+        this.str = str;
+    }
+    public void run(){
+        for(int i = 0; i < 10; i++){
+            System.out.println(str);
+
+            try{
+                Thread.sleep((int)(Math.random()*100));
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
